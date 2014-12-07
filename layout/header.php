98ebe9c7f42b905ff49b9703ca3f7c7bd45755a9
Arvind Singh <desiboyarvind@gmail.com>
2014-12-07 19:59:03 +0530

    Added initial files

---
+++ layout/header.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>AS Creations - Arvind hacker</title>
+    
+        <!-- including stylesheets -->
+        <link rel="stylesheet" href="<?php echo root; ?>inc/css/theme-v0.1.css">
+        
+        <!-- include javascripts -->
+        <script src="<?php echo root; ?>inc/js/script.js"></script>
+    </head>
+    <body>
+        <header>
+            <img src="img/logo.png">
+        </header>
+        <div class="menu">
+            <div id="body-holder">
+                <div class="right">
+                    <a href="">LOGIN</a>
+                    <a href="">REGISTER</a>
+                </div>
+                <a href="" class="current">HOME</a>
+                <a href="">SHOWCASE</a>
+                <a href="">PRICING</a>
+                <a href="">CONTACT</a>
+            </div>
+        </div>
+    
+    
---
+++ pricing.php
@@ -0,0 +1,44 @@
+<?php
+define('root','');
+include root.'layout/header.php';
+?>
+
+<div id="body-holder">
+    
+    <h1>Pricing</h1>
+    
+    <!-- placing 3 divs in line -->
+    
+    <div class="inline-table">
+        <div class="title">Basic Fix</div>
+        
+        <div class="row">Price $1</div>
+        <div class="row">Estimated dilevry in 24 hrs</div>
+        <div class="row">Quality Score: 7.0 +</div>
+        <div class="row">Free Revision: Unlimited</div>
+        
+    </div>
+    
+    <div class="inline-table">
+        <div class="title">Extensive Editing</div>
+        
+        <div class="row">Price $1</div>
+        <div class="row">Estimated dilevry in 24 hrs</div>
+        <div class="row">Quality Score: 7.0 +</div>
+        <div class="row">Free Revision: Unlimited</div>
+        
+    </div>
+    
+    <div class="inline-table">
+        <div class="title">Advanced Service</div>
+        
+        <div class="row">Price $1</div>
+        <div class="row">Estimated dilevry in 24 hrs</div>
+        <div class="row">Quality Score: 7.0 +</div>
+        <div class="row">Free Revision: Unlimited</div>
+        
+    </div>
+    
+</div>
+
+<?php include root.'layout/footer.php'; ?>
